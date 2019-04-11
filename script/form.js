79998cd0c569cd5586df0bcdc28c93825196978d
Ana Costa <luizacosta64@hotmail.com>
2019-04-11 20:40:55 -0300

    validacaoformulario

---
+++ script/form.js
@@ -0,0 +1,69 @@
+const $contatoInputNome = document.getElementById(`contatoInputNome`)
+const $contatoInputEmail = document.getElementById(`contatoInputEmail`)
+const $contatoInputTelefone = document.getElementById(`contatoInputTelefone`)
+const $contatoInputMessage = document.getElementById(`contatoInputMessage`)
+const $btn = document.getElementById(`btn`)
+const $form = document.querySelector('form')
+//presta atençao quando tiver(evento) uma ação sobre vocë
+//quando o usuario enviar o submit executa uma função
+form.addEventListener(`submit`, function (event) {
+    event.preventDefault()//previne que evento padrao ocorre
+
+    if (
+        //value e a propriedade do campo
+        $contatoInputNome.value === undefined ||
+        $contatoInputNome.value === null ||
+        $contatoInputNome.value === `` ||
+        $contatoInputNome.value === ` ` ||
+        $contatoInputNome.value != Number ||
+        $contatoInputNome.value.length < 2) {
+
+        $contatoInputNome.focus()
+        alert("Digite um nome válido")
+
+        return false
+    }
+
+    else if (
+        $contatoInputEmail.value === undefined ||
+        $contatoInputEmail.value === null ||
+        $contatoInputEmail.value === `` ||
+        $contatoInputEmail.value === ` ` ||
+        $contatoInputEmail.value.indexOf("@")==-1 ||
+        $contatoInputEmail.value.indexOf(".") == -1) {
+        $contatoInputEmail.focus()
+        alert("Digite um email válido")
+
+        return false
+    }
+
+    else if (
+        $contatoInputTelefone.value === undefined ||
+        $contatoInputTelefone.value === null ||
+        $contatoInputTelefone.value === `` ||
+        $contatoInputTelefone.value === ` ` ||
+        isNaN($contatoInputTelefone.value) ||
+        $contatoInputTelefone.value.length < 9) {
+
+        $contatoInputTelefone.focus()
+        alert("Digite um Telefone válido")
+
+        return false
+    }
+    else if (
+        $contatoInputMessage.value === undefined ||
+        $contatoInputMessage.value === null ||
+        $contatoInputMessage.value === `` ||
+        $contatoInputMessage.value === ` ` ||
+        $contatoInputMessage.value.length < 10) {
+
+        $contatoInputMessage.focus()
+        alert("Digite uma mensagem no mínimo 10 caracteres")
+
+        return false
+    }
+    document.querySelector('form').submit()
+    alert(`Todos os dados foram inseridos corretamente!`)
+
+
+})
